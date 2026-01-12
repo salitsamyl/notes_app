1c2e7c4158868f71574e35d4ca5485c9682fa62b
salitsamyl <salitsameylani@gmail.com>
2026-01-12 10:54:58 +0700

    Menghubungkan dashboard dengan notes_provider

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -13,93 +13,14 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<NotesProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Center(
         child: Text("Dashboard",style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
         backgroundColor: Colors.purple.shade100,),
 
-        floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final result = await Navigator.pushNamed(context, "/create");
-
-          if (result == true) {
-            showDialog(
-              context: context,
-              builder: (_) => AlertDialog(
-                title: const Text('Berhasil'),
-                content: const Text('Catatan berhasil ditambahkan'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: const Text('OK'),
-                  ),
-                ],
-              ),
-            );
-            setState(() {});
-          }
-        },
-        backgroundColor: Colors.purple.shade100,
-        child: const Icon(Icons.add, color: Colors.white),
-      ),
-
-      body: Padding(
-      padding: EdgeInsets.all(12),
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 180,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12,
-          childAspectRatio: 0.9,
-        ),
-        itemCount: provider.notes.length,
-        itemBuilder: (context, index) {
-          final note = provider.notes[index];
-
-          return GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, "/detail", arguments: note);
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(14),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.purple.shade100.withOpacity(0.3),
-                    blurRadius: 6,
-                    offset: Offset(2, 3),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      note.title,
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 6),
-                    Text(
-                      note.content,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    ),
+        body: ListView(),
     );
   }
 }
