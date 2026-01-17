10c7ffaf96ff401fbf2dacaa91504d81b5dfae44
rfadwutm <rifautami191@gmail.com>
2026-01-17 21:06:11 +0700

    Update auth_provider: tambah fitur logout & simpan user ID ke session

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/services/auth_services.dart';
+import 'package:notes_app/model/user_model.dart';
+import 'package:notes_app/services/session_service.dart';
 
 class AuthProvider extends ChangeNotifier {
   bool isLoading = false;
@@ -7,29 +9,33 @@ class AuthProvider extends ChangeNotifier {
 
   // LOGIN
   Future<bool> login(String email, String password) async {
-  isLoading = true;
-  errorMessage = null;
-  notifyListeners();
-
-  try {
-    final success = await AuthService.login(
-      email: email,
-      password: password,
-    );
-
-    if (!success) {
-      errorMessage = 'Email atau password anda salah';
-    }
-
-    return success; 
-  } catch (e) {
-    errorMessage = 'Tidak dapat terhubung ke server';
-    return false;
-  } finally {
-    isLoading = false;
+    isLoading = true;
+    errorMessage = null;
     notifyListeners();
+
+    try {
+      UserModel? user = await AuthService.login(
+        email: email,
+        password: password,
+      );
+
+      if (user == null) {
+        errorMessage = 'Email atau password salah';
+        return false;
+      }
+
+      // SIMPAN USER ID KE SESSION
+      await SessionService.saveUserId(user.id!);
+
+      return true;
+    } catch (e) {
+      errorMessage = 'Tidak dapat terhubung ke server';
+      return false;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
   }
-}
 
   // REGISTER
   Future<bool> register(
@@ -42,20 +48,31 @@ class AuthProvider extends ChangeNotifier {
     errorMessage = null;
     notifyListeners();
 
-    final success = await AuthService.register(
-      username: username,
-      name: name,
-      email: email,
-      password: password,
-    );
+    try {
+      final success = await AuthService.register(
+        username: username,
+        name: name,
+        email: email,
+        password: password,
+      );
 
-    isLoading = false;
+      if (!success) {
+        errorMessage = 'Email sudah terdaftar';
+      }
 
-    if (!success) {
-      errorMessage = 'Email yang Anda masukkan sudah terdaftar';
+      return success;
+    } catch (e) {
+      errorMessage = 'Tidak dapat terhubung ke server';
+      return false;
+    } finally {
+      isLoading = false;
+      notifyListeners();
     }
+  }
 
+  //LOGOUT
+  Future<void> logout() async {
+    await SessionService.clearSession();
     notifyListeners();
-    return success;
   }
 }
