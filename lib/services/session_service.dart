5ba31694be95ee63d4a3593b04ff80bc95e6edc4
syifaaawn <syifawahyuningsih25@gmail.com>
2026-01-17 13:44:54 +0700

    menambahkan fungsi logout pada session

--- lib/services/session_service.dart
+++ lib/services/session_service.dart
@@ -3,13 +3,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SessionService {
   static const _keyUserId = 'userId';
 
+  // simpan user id
   static Future<void> saveUserId(int userId) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt(_keyUserId, userId);
   }
 
+  // ambil user id
   static Future<int?> getUserId() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getInt(_keyUserId);
+  }
+
+  // logout
+  static Future<void> clearSession() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_keyUserId);
   }
 }
