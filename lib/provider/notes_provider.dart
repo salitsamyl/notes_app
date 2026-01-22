34fbc471ab2bcec2f7e5f47a18a5985feaf6bac6
syifaaawn <syifawahyuningsih25@gmail.com>
2026-01-22 17:51:45 +0700

    generate apk rilis

--- lib/provider/notes_provider.dart
+++ lib/provider/notes_provider.dart
@@ -5,7 +5,7 @@ import 'package:notes_app/model/notes_model.dart';
 import 'package:notes_app/services/session_service.dart';
 
 class NotesProvider extends ChangeNotifier {
-  final String baseUrl = 'http://192.168.1.16:3000/notes';
+  final String baseUrl = 'http://10.170.144.208:3000/notes';
 
   bool _isLoading = false;
   bool get isLoading => _isLoading;
